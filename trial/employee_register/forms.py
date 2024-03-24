7f93b715e3970ebd5a08bccbf513fa875b39aa21
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-24 23:37:35 +0545

    form model

---
+++ trial/employee_register/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Employee
+
+
+class EmployeeForm(forms.ModelForm):
+    
+    class Meta:
+        model=Employee
+        fields = ('full_name','emp_code','mobile_number','Position')
+        #this can also be given as
+        #fields=('fullname','emp_code','mobile','position')
+        labels={
+            'full_name':'FullName',
+            'emp_code':'EmployeeCode',
+        }
+
+        def __init__(self,*args,**kwargs):
+            super(EmployeeForm,self).__init__(*args,**kwargs)
+            self.fields['Position'].empty_label ="SELECT"
+            # self.fields['emp_code'].required= False
+            #this removes the requirement which is a part of form validation
