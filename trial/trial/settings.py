fdf5580ea7b4e83518566483fcaea04f22566b53
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-24 23:38:46 +0545

    added new apps

--- trial/trial/settings.py
+++ trial/trial/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee_register'
+    'employee_register',
+    'crispy_forms'
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
